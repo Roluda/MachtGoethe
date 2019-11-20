b6d495c34e0c7f16ee9ae261e49ed5d7927636ba
Robin Daraban <robin.daraban@gmx.de>
2019-11-21 00:17:11 +0100

    Add: noise, openBox, receiveWin, releaseBigMagnet

--- LichtsensorPrototyp/LichtsensorPrototyp.ino
+++ LichtsensorPrototyp/LichtsensorPrototyp.ino
@@ -1,48 +1,90 @@
 
-#define PHOTOSENSOR_GREEN A1
-#define PHOTOSENSOR_RED A0
-#define PHOTOSENSOR_BLUE A2
-#define LEDPIN 13
+#define PHOTOSENSOR_GREEN A3
+#define PHOTOSENSOR_RED A4
+#define PHOTOSENSOR_BLUE A5
+#define SUMMER 2
+#define BoxRelais A0
+#define DoneInput A1
+#define BigRelais A2
 
-int sensorValueGreen= 0;
+int sensorValueGreen = 0;
 int sensorValueRed = 0;
 int sensorValueBlue = 0;
 int greenThreshold = 50;
 int redThreshold = 100;
 int blueThreshold = 400;
 
+bool solved = false;
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LEDPIN, OUTPUT);
+  pinMode(SUMMER, OUTPUT);
+  pinMode(BoxRelais, OUTPUT);
+  pinMode(BigRelais, OUTPUT);
   Serial.begin(9600);
-
 }
 
 void loop() {
+  if (solved && digitalRead(DoneInput)) {
+    OpenBigMagnet();
+    solved = false;
+  }
   // put your main code here, to run repeatedly:
-  sensorValueGreen= analogRead(PHOTOSENSOR_GREEN);
-  sensorValueRed= analogRead(PHOTOSENSOR_RED);
-  sensorValueBlue= analogRead(PHOTOSENSOR_BLUE);
+  sensorValueGreen = analogRead(PHOTOSENSOR_GREEN);
+  sensorValueRed = analogRead(PHOTOSENSOR_RED);
+  sensorValueBlue = analogRead(PHOTOSENSOR_BLUE);
   bool greenBright = false;
   bool redBright = false;
   bool blueBright = false;
-  if(sensorValueGreen<greenThreshold){
-    greenBright=true;
+  int correctColors = 0;
+  if (sensorValueGreen < greenThreshold) {
+    correctColors ++;
+    greenBright = true;
     //Serial.println("YELLOW");
   }
-  if(sensorValueRed<redThreshold){
+  if (sensorValueRed < redThreshold) {
+    correctColors ++;
     redBright = true;
     //Serial.println("RED");
   }
-  if(sensorValueBlue<blueThreshold){
+  if (sensorValueBlue < blueThreshold) {
+    correctColors ++;
     blueBright = true;
     //Serial.println("BLUE");
   }
-  if(greenBright && blueBright && redBright){
+  for (int i = 0; i < correctColors; i++) {
+    Beep(200);
+  }
+  if (correctColors == 3) {
+    delay(1000);
+    OpenBox();
+    solved = true;
     //Serial.println("CONGRATULATIONSSSSS");
     //delay(3000);
   }
+
+
   //Serial.println(sensorValueGreen, DEC);
-  Serial.println(sensorValueRed, DEC);
+  //Serial.println(sensorValueRed, DEC);
   //Serial.println(sensorValueBlue, DEC);
 }
+
+void Beep(int milliseconds) {
+  digitalWrite(SUMMER, HIGH);
+  delay(milliseconds);
+  digitalWrite(SUMMER, LOW);
+  delay(milliseconds);
+}
+
+void OpenBox() {
+  digitalWrite(BoxRelais, HIGH);
+  delay(5000);
+  digitalWrite(BoxRelais, LOW);
+}
+
+void OpenBigMagnet() {
+  digitalWrite(BigRelais, HIGH);
+  delay(1000);
+  digitalWrite(BigRelais, LOW);
+}
+
